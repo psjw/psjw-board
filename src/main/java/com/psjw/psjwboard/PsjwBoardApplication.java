14dd15cbab156cf9165fef25a99024ba93823e1d
psjw <dlrudtn108@gmail.com>
2025-10-28 18:57:47 +0900

    init:project

---
+++ src/main/java/com/psjw/psjwboard/PsjwBoardApplication.java
@@ -0,0 +1,13 @@
+package com.psjw.psjwboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsjwBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsjwBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psjw/psjwboard/PsjwBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psjw.psjwboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsjwBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
